255ac0705dc66e3bf99a8fd6007e84376a1b90d8
Rohan <aitharohanlucky@gmail.com>
2025-11-04 09:17:04 +0530

    deploy5.2

--- backend/index.js
+++ backend/index.js
@@ -69,6 +69,10 @@ io.on("connection", (socket) => {
 });
 
 // Add Socket.IO route to verify it's working
+app.get("/socket.io/", (req, res) => {
+  res.send("Socket.IO endpoint is running");
+});
+
 
 
 // Existing routes
